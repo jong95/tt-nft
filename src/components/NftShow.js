362e6125e4b965147b1b109d946040912b99d2d0
Jeon Jong Hwan <jong95@gmail.com>
2022-03-01 20:41:25 +0900

    Hide contract input and use option for token Id.

--- src/components/NftShow.js
+++ src/components/NftShow.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { useNft } from 'use-nft';
+import Mint from './Mint';
 
 const NftShow = ({ contractAddress, tokenId }) => {
   console.log('contractAddress: ', contractAddress);
   console.log('tokenId: ', tokenId);
 
-  const { loading, error, nft } = useNft(
-    // '0x2789eE502f55643Eb6a7431170Fb5d7B03A6CA5f',
-    // '1',
-    contractAddress,
-    tokenId,
-  );
+  const { loading, error, nft } = useNft(contractAddress, tokenId);
 
   // nft.loading is true during load.
   if (loading) return <>Loading…</>;
 
   // nft.error is an Error instance in case of error.
-  console.log('error: ', error);
-  console.log('nft: ', nft);
-  if (error || !nft) return <>Error. (Maybe not yet minted.)</>;
+  // console.log('error: ', error);
+  // console.log('nft: ', nft);
+  if (error || !nft) {
+    return (
+      <>
+        <h3>Not yet minted.</h3>
+        <Mint />
+      </>
+    );
+  }
 
   // You can now display the NFT metadata.
   return (
--- src/components/NftView.js
+++ src/components/NftView.js
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import { providers, getDefaultProvider } from 'ethers';
 import { NftProvider } from 'use-nft';
 import NftShow from './NftShow';
-import Mint from './Mint';
+import { Container, Col, Row } from 'react-bootstrap';
 
 const ethersConfig = {
   // provider: getDefaultProvider('rinkeby', {
@@ -10,7 +11,7 @@ const ethersConfig = {
 
   provider: new providers.AlchemyProvider(
     'matic',
-    'aPaDbb6ZM5j14sr3tKo4m4B3VlCCeN4u',
+    process.env['ACCOUNT_PRIVATE_KEY'],
   ),
 };
 
@@ -23,10 +24,7 @@ const NftView = ({
   NftContractAddress,
   NftTokenId,
 }) => {
-  const onSubmit = (e) => {
-    e.preventDefault();
-    getNftData(e.target[0].value, e.target[1].value);
-  };
+  const nftContractAddress = '0x6A544c126fFdE8E4e9cBF1A4Dfd0883C0639eb90';
 
   const onChangeContractAddress = (e) =>
     onChangeContractAddressInput(e.target.value);
@@ -35,34 +33,45 @@ const NftView = ({
   // console.log('build Nft component');
   // console.log('NftContractAddress: ', NftContractAddress);
   // console.log('NftTokenId: ', NftTokenId);
+  const tokenIdOptions = [];
+  for (let i = 1; i < 11; ++i) tokenIdOptions.push(i);
+  const [selectedOption, setSelectedOption] = useState(tokenIdOptions[0].value);
 
   return (
     <NftProvider fetcher={['ethers', ethersConfig]}>
       <div>
-        <form onSubmit={onSubmit}>
-          <h3>Nft Smart Contract Address</h3>
-          <h6>
-            Sample NFT Contract (on Matic Network) : <br />
-            0x6A544c126fFdE8E4e9cBF1A4Dfd0883C0639eb90
-          </h6>
-          <input
-            style={{ width: '350px' }}
-            value={contractAddress}
-            onChange={onChangeContractAddress}
-          />
-          <br />
-          <h3>Nft Token ID</h3>
-          <input
-            style={{ width: '50px' }}
-            value={tokenId}
-            onChange={onChangeTokenId}
-          />
-          <br />
-          <button type="submit">View</button>
-        </form>
+        <Container>
+          <Row>
+            <Col>
+              <h3>Nft Smart Contract Address</h3>
+              <h6>
+                Sample NFT Contract (on Matic Network) : <br />
+                0x6A544c126fFdE8E4e9cBF1A4Dfd0883C0639eb90
+              </h6>
+              <h3>Nft Token ID</h3>
+              <select
+                value={selectedOption}
+                onChange={(e) => {
+                  setSelectedOption(e.target.value);
+                  getNftData(nftContractAddress, e.target.value);
+                }}
+              >
+                {tokenIdOptions.map((tokenId) => (
+                  <option key={tokenId} value={tokenId}>
+                    #{tokenId}
+                  </option>
+                ))}
+              </select>
+            </Col>
+            <Col>
+              <NftShow
+                contractAddress={NftContractAddress}
+                tokenId={NftTokenId}
+              />
+            </Col>
+          </Row>
+        </Container>
       </div>
-      <Mint />
-      <NftShow contractAddress={NftContractAddress} tokenId={NftTokenId} />
     </NftProvider>
   );
 };
