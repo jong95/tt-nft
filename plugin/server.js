1de77e9912172f6ed03ac71b75523a76b7b80166
jong95 <jong95@gmail.com>
2022-03-21 17:52:16 +0900

    Fix error.

--- plugin/server.js
+++ plugin/server.js
@@ -94,7 +94,7 @@ app.get('/ttnft/:id', (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log(
     'Express server listening on port %d in %s mode',
-    this.address().port,
+    process.env.PORT,
     app.settings.env,
   );
 });
