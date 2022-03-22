2d3acb495967ad5761333568f7280925e875e2a0
jong95 <jong95@gmail.com>
2022-03-22 18:57:34 +0900

    Add web client redirection.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,52 @@ import './App.css';
 import NftDataContainer from './containers/NftDataContainer';
 import { Button } from 'react-bootstrap';
 import useMetaMask from './hooks/metamask';
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 function App() {
   const { connect, disconnect, isActive, account, shouldDisable, chainId } =
     useMetaMask();
+
+  const data = [
+    {
+      // actionType: 'show-toast',
+      // actionData: { toastType: 'success', toastMessage: 'Successfully voted!' },
+      actionType: 'append',
+      actionData: [
+        {
+          blockId: 'button',
+          blocks: [
+            {
+              type: 'text',
+              subType: 'h1',
+              color: '#b1b1b1',
+              content: 'DONE',
+              botId: 'B1PAE2EDV',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  const query = useQuery();
+  console.log('tokenId: ', query.get('tokenId'));
+  console.log('blockId: ', query.get('blockId'));
+  console.log('actionId: ', query.get('actionId'));
+
+  const handleClick = () => {
+    window.location.replace(
+      `https://padobox.vingle.network/plugin/revised/callback?data=${JSON.stringify(
+        data,
+      )}`,
+    );
+  };
 
   return (
     <div>
@@ -21,7 +63,10 @@ function App() {
         Disconnect MetaMask
       </Button>
       <hr />
-      <NftDataContainer />
+      {/* <NftDataContainer /> */}
+      <Button onClick={handleClick} type="button">
+        Redirect
+      </Button>
     </div>
   );
 }
