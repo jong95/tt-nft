bd084f5c4df24b8ac324a4b0887d42a00d991ee5
jong95 <jong95@gmail.com>
2022-03-23 11:08:34 +0900

    Rearrange code.

--- plugin/server.js
+++ plugin/server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/home', (req, res) => {
-  console.log('call /home');
-  console.log('req.body: ', req.body);
+  // console.log('call /home');
+  // console.log('req.body: ', req.body);
 
   const block = {
     blocks: [
@@ -31,10 +31,10 @@ app.post('/action', (req, res) => {
   const blockId = req.body.data.params.blockId;
   const tokenId = req.body.data.params.tokenId;
 
-  console.log('call /action');
-  console.log('actionId: ', actionId);
-  console.log('blockId: ', blockId);
-  console.log('tokenId: ', tokenId);
+  // console.log('call /action');
+  // console.log('actionId: ', actionId);
+  // console.log('blockId: ', blockId);
+  // console.log('tokenId: ', tokenId);
 
   const block = {
     data: [
@@ -51,57 +51,18 @@ app.post('/action', (req, res) => {
 });
 
 app.post('/replace', (req, res) => {
-  const replaceId = req.body.replaceId;
+  // const replaceId = req.body.replaceId;
   const blockId = req.body.data[0].blockId;
   const tokenId = parseInt(req.body.data[0].params.tokenId);
 
-  console.log('replaceId: ', replaceId);
-  console.log('blockId: ', blockId);
-  console.log('tokenId: ', tokenId);
+  // console.log('replaceId: ', replaceId);
+  // console.log('blockId: ', blockId);
+  // console.log('tokenId: ', tokenId);
 
   const web3 = alchemy.createAlchemyWeb3(
     'https://polygon-mainnet.g.alchemy.com/v2/5HELYofdzUhXvcGCDJdFzpmZNU0uV04n',
   );
 
-  // Normal case response.
-
-  // 	{
-  //   contract: { address: '0x6a544c126ffde8e4e9cbf1a4dfd0883c0639eb90' },
-  //   id: { tokenId: '1', tokenMetadata: { tokenType: 'ERC721' } },
-  //   title: '#1 NFT',
-  //   description: 'NFT description',
-  //   tokenUri: {
-  //     raw: 'https://thirsteez-data.s3.ap-northeast-2.amazonaws.com/metadata/json/1',
-  //     gateway: 'https://thirsteez-data.s3.ap-northeast-2.amazonaws.com/metadata/json/1'
-  //   },
-  //   media: [
-  //     {
-  //       raw: 'https://thirsteez-data.s3.ap-northeast-2.amazonaws.com/metadata/image/1.jpg',
-  //       gateway: 'https://thirsteez-data.s3.ap-northeast-2.amazonaws.com/metadata/image/1.jpg'
-  //     }
-  //   ],
-  //   metadata: {
-  //     name: '#1 NFT',
-  //     description: 'NFT description',
-  //     image: 'https://thirsteez-data.s3.ap-northeast-2.amazonaws.com/metadata/image/1.jpg',
-  //     attributes: [ [Object] ]
-  //   },
-  //   timeLastUpdated: '2022-03-21T07:42:23.195Z'
-  // }
-
-  // Error case response.
-
-  // {
-  //   contract: { address: '0x6A544c126fFdE8E4e9cBF1A4Dfd0883C0639eb90' },
-  //   id: { tokenId: '10', tokenMetadata: { tokenType: 'ERC721' } },
-  //   title: '',
-  //   description: '',
-  //   tokenUri: { raw: 'Unable to get token URI', gateway: '' },
-  //   media: [ { raw: '', gateway: '' } ],
-  //   metadata: {},
-  //   timeLastUpdated: '',
-  //   error: 'Token does not exist'
-  // }
   web3.alchemy
     .getNftMetadata({
       contractAddress,
@@ -112,29 +73,22 @@ app.post('/replace', (req, res) => {
       let attributeBlocks = [];
 
       if (response.error === undefined) {
-        console.log('Found tokenId for NFT');
+        // console.log('Found tokenId for NFT');
+
         const name = response.metadata.name;
         const description = response.metadata.description;
         const image = response.metadata.image;
 
         response.metadata.attributes.map((attribute) => {
-          Object.keys(attribute).map((key, idx) => {
-            console.log('key: ', key);
-            console.log('attribute[key]: ', attribute[key]);
-            console.log('idx: ', idx);
+          Object.keys(attribute).map((key) => {
+            const content = `${key}: ${attribute[key]}`;
+
             attributeBlocks.push({
               type: 'text',
+              content,
+              botId: 'B1PAE2EDV',
               subType: 'h1',
               color: '#b1b1b1',
-              content: key,
-              botId: 'B1PAE2EDV',
-            });
-            attributeBlocks.push({
-              type: 'text',
-              subType: 'h1',
-              color: '#b1b1b1',
-              content: attribute[key],
-              botId: 'B1PAE2EDV',
             });
           });
         });
@@ -149,17 +103,17 @@ app.post('/replace', (req, res) => {
                   blocks: [
                     {
                       type: 'text',
+                      content: name,
                       subType: 'h1',
                       color: '#b1b1b1',
-                      content: name,
-                      botId: 'B1PAE2EDV',
+                      botId,
                     },
                     {
                       type: 'text',
+                      content: description,
                       subType: 'h2',
                       color: '#b1b1b1',
-                      content: description,
-                      botId: 'B1PAE2EDV',
+                      botId,
                     },
                     {
                       type: 'image',
@@ -167,14 +121,14 @@ app.post('/replace', (req, res) => {
                     },
                     ...attributeBlocks,
                   ],
-                  botId: 'B1PAE2EDV',
+                  botId,
                 },
               ],
             },
           ],
         };
       } else {
-        console.log('No tokenId for NFT');
+        // console.log('No tokenId for NFT');
 
         block = {
           data: [
@@ -186,14 +140,16 @@ app.post('/replace', (req, res) => {
                   blocks: [
                     {
                       type: 'text',
+                      content: 'Token does not exists',
+                      botId,
                       subType: 'h1',
                       color: '#b1b1b1',
-                      content: 'Token does not exists',
-                      botId: 'B1PAE2EDV',
                     },
                     {
+                      type: 'button',
+                      content: 'Mint NFT',
                       actionId: 'mintNFT',
-                      botId: 'B1PAE2EDV',
+                      botId,
                       element: {
                         type: 'flat',
                         size: 'medium',
@@ -205,18 +161,16 @@ app.post('/replace', (req, res) => {
                         tokenId,
                         blockId,
                       },
-                      content: 'Mint NFT',
-                      type: 'button',
                     },
                   ],
-                  botId: 'B1PAE2EDV',
+                  botId,
                 },
               ],
             },
           ],
         };
       }
-      console.log(response);
+
       res.send(block);
     });
 });
